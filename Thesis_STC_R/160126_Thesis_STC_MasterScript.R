f9099917b76e1a5eee4344fa26741fa755d7685e
roeland.dekoning <roeldk14@gmail.com>
2016-01-26 15:21:39 +0100

    Initial Master Script and Git Setup

---
+++ Thesis_STC_R/160126_Thesis_STC_MasterScript.R
@@ -0,0 +1,37 @@
+### Master Script Space_Time_Cube_Animal_Visualization 
+### 26\01\16 
+
+### check working directory
+
+getwd()
+
+### If required change directory
+
+# setwd()
+
+### Install required packages
+
+# install.packages("SpatialEpi")
+# install.packages("ks")
+# install.packages("zoom")
+# install.packages("geotools")
+# install.packages("rgdal")
+# install.packages("sp")
+# install.packages("rgeos")
+# install.packages("ggplot2")
+
+### Load Libraries
+
+library("SpatialEpi")
+library("ks")
+library("zoom")
+library("geotools")
+library("rgeos")
+library("rgdal")
+library("sp")
+library("ggplot2")
+
+### Load Functions
+
+#Source("R/")
+
