51c06264ac158c74d18ba110f95011492ddd504b
roeland.dekoning <roeldk14@gmail.com>
2016-02-16 12:12:00 +0100

    deletion of previous R script formatting

--- Thesis_STC_R/160215_Space_Time_Cube_Visualzer_Script.R
+++
@@ -1,2 +0,0 @@
-### Space Time Cube Visualzer Script  Space_Time_Cube_Animal_Visualization 
-### 15\02\16 
--- Thesis_STC_R/160216_Population_Averages_Pathway_Calculator.R
+++
@@ -1,7 +0,0 @@
-### Population_Averages_Pathway_Calculator_Script Space_Time_Cube_Animal_Visualization 
-### 16\02\16
-
-### Calculates the population or averages pathway of multiple individuals from a given dataset
-
-Averages_Pathway_Calculator <- function(dataset) {}
-
